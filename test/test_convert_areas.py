ac2348766548fc3b6d8fabb85568b6f567dca7cb
Yuto Watanabe <yuto.w51942@gmail.com>
2020-06-07 01:12:10 +0900

    出力コードをみれるようにテストコードを変更

--- test/test_convert_areas.py
+++ test/test_convert_areas.py
@@ -5,6 +5,7 @@
 Copyright (c) 2020 Earthquake alert
 '''
 import json
+import os
 
 from src.python.convert_areas import convert
 
@@ -12,11 +13,12 @@ from src.python.convert_areas import convert
 def test_convert():
     db_file_path = 'src/external/area-code-database/src/areas.db'
     image_file_path = 'src/cache'
+    output_file_path = os.path.join(image_file_path, 'output.json')
     earthquake = json_read('test/example/1.json')
 
     output = convert(earthquake, db_file_path, image_file_path)
 
-    print(output)
+    json_write(output_file_path, output)
 
 
 def json_read(json_file_path: str):
@@ -24,3 +26,8 @@ def json_read(json_file_path: str):
         json_body = json.load(contents)
 
     return json_body
+
+
+def json_write(json_file_path: str, json_body) -> None:
+    with open(json_file_path, mode='w') as contents:
+        json.dump(json_body, contents, indent=4, ensure_ascii=False)
