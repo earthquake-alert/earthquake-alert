48995d7bcb465e0716e420c756a6e4daa96da0ef
Yuto Watanabe <yuto.w51942@gmail.com>
2020-06-07 16:00:46 +0900

    並行処理の実装

---
+++ src/python/main.py
@@ -0,0 +1,26 @@
+'''
+@author: Yuto Watanabe
+@version: 1.0.0
+
+Copyright (c) 2020 Earthquake alert
+'''
+import time
+
+from convert_areas import convert
+from json_operation import json_read
+
+
+def test_convert():
+    db_file_path = 'src/external/area-code-database/src/areas.db'
+    image_file_path = 'src/cache'
+    earthquake = json_read('test/example/1.json')
+
+    output = convert(earthquake, db_file_path, image_file_path)
+    print(output)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    test_convert()
+    elapsed_time = time.time() - start
+    print(f'Time: {elapsed_time}[sec]')
